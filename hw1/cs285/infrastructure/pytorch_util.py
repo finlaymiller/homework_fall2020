bc58bec5754074c5c3239012152c68d037d0a68b
Finlay Miller <finlay@dal.ca>
2021-03-06 20:08:30 -0400

    Filled-in TODOs for Q1.1

--- hw1/cs285/infrastructure/pytorch_util.py
+++ hw1/cs285/infrastructure/pytorch_util.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from typing import Union
 
 import torch
@@ -47,7 +48,15 @@ def build_mlp(
 
     # TODO: return a MLP. This should be an instance of nn.Module
     # Note: nn.Sequential is an instance of nn.Module.
-    raise NotImplementedError
+    layers = [('linear1', nn.Linear(input_size, size)), ('activation1', activation)]
+    for i in range(2, n_layers+1):
+        layers.append((f'linear{i}', nn.Linear(size, size)))
+        layers.append((f'activation{i}', activation))
+
+    layers.extend([(f'linear{n_layers+1}', nn.Linear(size, output_size)), (f'activation{n_layers+1}', output_activation)])
+    model = nn.Sequential(OrderedDict(layers))
+
+    return model
 
 
 device = None
--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -81,7 +81,10 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             observation = obs[None]
 
         # TODO return the action that the policy prescribes
-        raise NotImplementedError
+        if self.discrete:
+            return ptu.to_numpy(self.logits_na(ptu.from_numpy(observation)))
+        else:
+            return ptu.to_numpy(self.mean_net(ptu.from_numpy(observation)))
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -93,7 +96,7 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor) -> Any:
-        raise NotImplementedError
+        return self.logits_na(observation) if self.discrete else self.mean_net(observation)
 
 
 #####################################################
@@ -109,7 +112,12 @@ class MLPPolicySL(MLPPolicy):
             adv_n=None, acs_labels_na=None, qvals=None
     ):
         # TODO: update the policy and return the loss
-        loss = TODO
+        # modified from https://pytorch.org/docs/stable/optim.html#taking-an-optimization-step
+        self.optimizer.zero_grad()
+        current_action = self.forward(ptu.from_numpy(observations))
+        loss = self.loss(current_action, ptu.from_numpy(actions))
+        loss.backward()
+        self.optimizer.step()
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
